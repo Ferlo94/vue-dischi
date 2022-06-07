f15152f51cf9ef3116be299c636f610aa1429069
Ferlo94 <100360746+Ferlo94@users.noreply.github.com>
2022-06-07 19:38:45 +0200

    header + logo

---
+++ src/components/blocks/BaseHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <header class="header">
+        <img src="../../assets/style/IMG/spotify-logo.png" alt="">
+    </header>
+</template>
+
+<script>
+
+
+export default {
+    name: 'BaseHeader',
+}
+</script>
+
+
+<style lang='scss' scoped>
+    header {
+        display: flex;
+        height: 50px;
+        background-color: #2e3a46;
+
+        img {
+            padding: 5px;
+        }
+    }
+</style>
